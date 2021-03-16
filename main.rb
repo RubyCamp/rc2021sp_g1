b727893219c1e36e46a0817012e5167065ed16f4
yu1k <48705423+yu1k@users.noreply.github.com>
2021-03-16 15:54:27 +0900

    Add : 2秒ごとにアニメーションをする処理を追加

--- main.rb
+++ main.rb
@@ -5,16 +5,13 @@ Window.width = 800
 Window.height = 600
 Window.caption = "キャラクター表示"
 
-image = Image.load("./image/car.png")
-# 画像の背景を透明に
-image.set_color_key(C_WHITE)
-
-player = Player.new(100, 100, image, 5)
+player = Player.new(100, 100, 5)
 
 Window.loop do
     # escキーを押すと、ループを抜ける処理
     break if Input.key_push?(K_ESCAPE)
 
+    # ↑キーを実行するとアニメーションが変わる
     player.update()
     player.draw()
 end
