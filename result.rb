a434faec1672590aa31adef66e9d9f98f94af9b7
Natsuki Okubo <natukiookubo15@gmail.com>
2021-03-18 13:54:54 +0900

    change a image

--- result.rb
+++ result.rb
@@ -3,32 +3,36 @@ require "dxruby"
 Window.height = 600
 Window.width = 800
 
-SCORE = "790000"
+SCORE = "75656"
 
 
 
 class Result
     
     def draw_result
-        font1 = Font.new(43)#dai/2 han
+        font1 = Font.new(45)#dai/2 han
         font2 = Font.new(100)
-        font3 = Font.new(24)
+        font3 = Font.new(35)
+        font4 = Font.new(24)
 
+
+        #スコアを中央に配置する
         s_size = SCORE.size
         s = 50*(s_size/2)
 
         
         # Window.draw_box_fill(0,0,800,600,[230,255,255]z=0)
-        Window.draw_font(335,100,"Result!",font1)
-        Window.draw_font(400-s,250,"#{SCORE}",font2)
-        Window.draw_font(270,500,"Press 'Space' to Restart!",font3)
+        Window.draw_font(335,100,"Result!",font1,)
+        Window.draw_font(390-s,250,"#{SCORE}",font2)
+        Window.draw_font(500,350,"Points!",font3)
+        Window.draw_font(270,500,"Press 'Space' to Restart!",font4)
 
     end
     ONE_SEC_FRAMES = 60
     def initialize()
-        @image = Image.load("images/background_image.png")
+        @image = Image.load("images/resultB.png")
 
-        @speed = 5
+        @speed = 3
         @map_x = 0
     end
 
